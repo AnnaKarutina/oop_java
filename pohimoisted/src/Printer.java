e02ec72b88baebe253c3b88e4dca5a4c6efd5d49
anna.karutina <anna.karutina@khk.ee>
2018-11-29 10:59:28 +0200

    Täiendati Printer paberi lisamise võimalusega

--- pohimoisted/src/Printer.java
+++ pohimoisted/src/Printer.java
@@ -37,8 +37,22 @@ public class Printer {
            paberiKulu = lehtedeArv / 2 + lehtedeArv % 2;
             System.out.println("kahepoolne printimine");
         }
-        this.paberPrintimiseks = this.paberPrintimiseks - paberiKulu;
-        return paberiKulu;
+        if(paberiKulu > this.paberPrintimiseks){
+            System.out.println("Printeris ei jätku paberit");
+            System.out.println("Lisa " + (paberiKulu - this.paberPrintimiseks) + " paberit juurde");
+            return 0;
+        } else {
+            this.paberPrintimiseks = this.paberPrintimiseks - paberiKulu;
+            return paberiKulu;
+        }
+    }
+
+    public Integer lisaPaber(Integer lehtedeArv){
+        if(lehtedeArv > 0 & lehtedeArv < 500) {
+            this.paberPrintimiseks = this.paberPrintimiseks + lehtedeArv;
+            System.out.println("Printerisse on lisatud " + lehtedeArv + " paberit");
+        }
+        return this.paberPrintimiseks;
     }
 
     public Integer getPaberPrintimiseks() {
--- pohimoisted/src/PrinterTest.java
+++ pohimoisted/src/PrinterTest.java
@@ -5,5 +5,8 @@ public class PrinterTest {
         System.out.println("Printeri tooneri tase on " + printer1.getTooneriTase() + "%");
         Integer kontrolltoo = printer1.prindi(5);
         System.out.println("Printisin kontrolltöö jaoks " + kontrolltoo + " paberi lehte ja printeris on jäänud " + printer1.getPaberPrintimiseks() + " paberi lehte");
+        printer1.lisaPaber(50);
+        kontrolltoo = printer1.prindi(5);
+        System.out.println("Printisin kontrolltöö jaoks " + kontrolltoo + " paberi lehte ja printeris on jäänud " + printer1.getPaberPrintimiseks() + " paberi lehte");
     }
 }
