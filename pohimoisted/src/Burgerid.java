3f7cee1232c1a119a36685e70c3ee3d53ec2e823
anna.karutina <anna.karutina@khk.ee>
2018-12-06 09:25:34 +0200

    loodud ja testitud LuxBurgeri

--- pohimoisted/src/Burgerid.java
+++ pohimoisted/src/Burgerid.java
@@ -7,11 +7,20 @@ public class Burgerid {
         Double hamburgerihind = hamburger.koostaBurger();
         System.out.println("Lõpphind = " + hamburgerihind);
         System.out.println("======================================");
+
         TervislikBurger kalaburger = new TervislikBurger("kala", 4.0);
         kalaburger.valiTervislikLisand1("salat", 0.5);
         kalaburger.valiTervislikLisand2("kaste", 0.2);
         kalaburger.valiLisand1("oliivid", 0.3);
         Double kalaburgeriHind = kalaburger.koostaBurger();
         System.out.println("Lõpphind = " + kalaburgeriHind);
+        System.out.println("======================================");
+
+        LuxBurger lux = new LuxBurger();
+        lux.valiLisand1("salat", 0.5);
+        lux.valiLisand2("kaste", 0.2);
+        lux.valiLisand3("oliivid", 0.3);
+        Double luxburgeriHind = lux.koostaBurger();
+        System.out.println("Lõpphind = " + luxburgeriHind);
     }
 }
